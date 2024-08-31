3acef3b31eb3cad2454c864b69c9627013a752a8
Crowdbotics <team@crowdbotics.com>
2024-08-31 18:59:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gobook-49241",
+  url: "https://gobook-49241.botics.co",
+  api: "https://gobook-49241.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
